da15d6827a461bb0f903b02e15be805978e43afc
nulLeeKH <i_am@nulleekh.com>
2020-03-07 16:12:10 +0900

    [ADD] 66-day/MainViewController.swift

---
+++ 66-day/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  66-day
+//
+//  Created by Kyung-ha Lee on 2020/03/07.
+//  Copyright © 2020 Kyung-ha Lee. All rights reserved.
+//
+
+import Cocoa
+
+class MainViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}

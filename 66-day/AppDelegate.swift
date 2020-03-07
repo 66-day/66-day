66cb8296ba82aa24dd7fb1ec6ea7d8a986200bf1
nulLeeKH <i_am@nulleekh.com>
2020-03-07 12:32:12 +0900

    [ADD] 66-day/* and 66-day.xcodeproj/*

---
+++ 66-day/AppDelegate.swift
@@ -0,0 +1,62 @@
+//
+//  AppDelegate.swift
+//  66-day
+//
+//  Created by Kyung-ha Lee on 2020/03/07.
+//  Copyright © 2020 Kyung-ha Lee. All rights reserved.
+//
+
+/*
+ MIT License
+
+ Copyright (c) 2019 Kyung-ha Lee <i_am@nulleekh.com>
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+import Cocoa
+import SwiftUI
+
+
+enum InterfaceStyle : String {
+   case Dark, Light
+
+   init() {
+      let type = UserDefaults.standard.string(forKey: "AppleInterfaceStyle") ?? "Light"
+      self = InterfaceStyle(rawValue: type)!
+    }
+}
+
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        if let button = statusItem.button {
+            button.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
+        }
+    }
+    
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+}
